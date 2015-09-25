811737f16d30890016e2aa6b9df568cb8982804a
davide <thekage91@gmail.com>
2015-09-25 17:03:36 +0200

    Add condition for select domemo and mood

--- context.js
+++ context.js
@@ -30,7 +30,48 @@ var create_context_data = function(){
 		var mood = "";
 		var domEmo = "";
 
-		if()
+		/* MOOD */
+		if(i <= 504){
+			mood = "neutral";
+		}
+
+		if(505 <= i <= 1345){
+			mood = "positive";
+		}
+
+		if(1346 <= i <= 1682){
+			mood = "negative"
+		}
+		/********/
+
+		if(i <= 336){
+			domEmo = "sad";
+		}
+
+		if( 337 <= i <= 504){
+			domEmo = "happy";
+		}
+
+		if( 505 <= i <= 673){
+			domEmo = "scared";
+		}
+
+		if( 674 <= i <= 1010 ){
+			domEmo = "surprised";
+		}
+
+		if( 1011 <= i <= 1191 ){
+			domEmo = "angry";
+		}
+
+		if( 1192 <= i <= 1528 ){
+			domEmo = "disgusted;"
+		}
+
+		if( 1529 <= i <= 1682 ){
+			domEmo = "neutral";
+		}
+
 		client.hgetall("movieId-" + i, function(err, movie){
 
 			for(var key in object){
@@ -40,8 +81,8 @@ var create_context_data = function(){
 
 					"movieId": i,
 					"rating": movie_info.rating,
-					"mood": 
-					"domEmo":
+					"mood": mood,
+					"domEmo":domEmo
 				};
 
 				client.hmset(key, "ratedMovie-" + i, JSON.stringify(movieObj));
